6713f7ae6b04e41f7ccae99df038c0c438474c87
devjaeyoon <devjaeyoon@gmail.com>
2025-05-28 15:04:48 +0900

    refactor: 즐겨찾기 토글 핸들러 이름을 onToggleInterest에서 onToggleFavorite로 변경
    - isFavoriteChannel prop과 네이밍 일관성을 맞추기 위해 onToggleInterest를 onToggleFavorite로 변경
    - 의미 명확화를 위한 네이밍 리팩터링이며, 기능 동작에는 변화 없음

--- src/components/ChannelListItem.jsx
+++ src/components/ChannelListItem.jsx
@@ -6,7 +6,7 @@ const ChannelListItem = ({
   thumbnail,
   channelName,
   isFavoriteChannel,
-  onToggleInterest,
+  onToggleFavorite,
 }) => {
   return (
     <li className="my-2 flex h-16 w-[352px] items-center rounded-md bg-gray-100 p-2">
@@ -15,7 +15,7 @@ const ChannelListItem = ({
         <>
           <p className="ml-3 w-3/5 text-sm font-bold">{channelName}</p>
           <div className="flex">
-            <button onClick={onToggleInterest}>
+            <button onClick={onToggleFavorite}>
               <FilledStar className="mr-1 mb-0.5 ml-2" />
             </button>
             <button>
@@ -26,7 +26,7 @@ const ChannelListItem = ({
       ) : (
         <>
           <p className="ml-3 w-3/4 text-sm font-bold">{channelName}</p>
-          <button onClick={onToggleInterest}>
+          <button onClick={onToggleFavorite}>
             <BlankStar className="mx-3 ml-auto" />
           </button>
         </>
