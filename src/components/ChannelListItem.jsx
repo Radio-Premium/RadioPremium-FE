f8130cf6d52fbc19b470dfc280e56eeeb6f7d0f6
이재윤 <devjaeyoon@gmail.com>
2025-05-28 16:25:44 +0900

    feat: 라디오 채널 목록 UI 구현 (#6)
    * feat: 채널 목록 관련 svg 이미지 추가
    
    * feat: ChannelListItem 컴포넌트 UI 구현
    
    - 라디오 채널 목록 아이템 컴포넌트(ChannelListItem) 추가
    - 즐겨찾기 여부(isFavoriteChannel)에 따라 아이콘 조건부 렌더링
    - 즐겨찾기 채널인 경우 DragDrop 아이콘 표시
    
    * feat: ChannelList 컴포넌트 즐겨찾기 토글 기능 구현
    
    - 즐겨찾기 버튼 클릭 시 onToggleInterest 콜백 호출
    
    * refactor: 즐겨찾기 토글 핸들러 이름을 onToggleInterest에서 onToggleFavorite로 변경
    
    - isFavoriteChannel prop과 네이밍 일관성을 맞추기 위해 onToggleInterest를 onToggleFavorite로 변경
    - 의미 명확화를 위한 네이밍 리팩터링이며, 기능 동작에는 변화 없음
    
    * refactor: 채널 썸네일 이미지에 alt 속성 추가로 접근성 개선
    
    - <img> 태그에 alt={} 속성 추가
    - 시각 장애인을 위한 스크린 리더 대응
    - 시멘틱한 마크업 구성으로 접근성 향상
    
    * rename: SVG 컴포넌트 이름을 \*Icon 접미사로 변경하여 충돌 방지

---
+++ src/components/ChannelListItem.jsx
@@ -0,0 +1,42 @@
+import BlankStar from "@/assets/svgs/BlankStar.svg?react";
+import DragDrop from "@/assets/svgs/DragDrop.svg?react";
+import FilledStar from "@/assets/svgs/FilledStar.svg?react";
+
+const ChannelListItem = ({
+  thumbnail,
+  channelName,
+  isFavoriteChannel,
+  onToggleFavorite,
+}) => {
+  return (
+    <li className="my-2 flex h-16 w-[352px] items-center rounded-md bg-gray-100 p-2">
+      <img
+        className="h-12 w-12"
+        src={thumbnail}
+        alt={`${channelName} 썸네일`}
+      />
+      {isFavoriteChannel ? (
+        <>
+          <p className="ml-3 w-3/5 text-sm font-bold">{channelName}</p>
+          <div className="flex">
+            <button onClick={onToggleFavorite}>
+              <FilledStar className="mr-1 mb-0.5 ml-2" />
+            </button>
+            <button>
+              <DragDrop className="ml-3" />
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <p className="ml-3 w-3/4 text-sm font-bold">{channelName}</p>
+          <button onClick={onToggleFavorite}>
+            <BlankStar className="mx-3 ml-auto" />
+          </button>
+        </>
+      )}
+    </li>
+  );
+};
+
+export default ChannelListItem;
