4f7c1d7c249b937188c1f832ecc07858af622afa
h-faber <hfaberlu@gmail.com>
2025-05-29 04:05:11 +0900

    feat: ToggleCheckbox에서 상위 체크 해제 시 하위 선택 초기화 기능 추가

--- src/components/ToggleCheckbox.jsx
+++ src/components/ToggleCheckbox.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Checkbox from "./ui/Checkbox";
 
 const ToggleCheckbox = ({
@@ -6,11 +8,22 @@ const ToggleCheckbox = ({
   parentOption,
   childrenOptions,
 }) => {
+  const [selectedChildId, setSelectedChildId] = useState(null);
+
+  const handleParentClick = () => {
+    onSelect(); // 부모 선택 상태 변경
+    setSelectedChildId(null); // 자식 선택 초기화
+  };
+
+  const handleChildClick = (id) => {
+    setSelectedChildId((prev) => (prev === id ? null : id));
+  };
+
   return (
     <>
       <Checkbox
         checked={isSelected}
-        onChange={onSelect}
+        onChange={handleParentClick}
         className="mt-[10px] text-[18px]"
       >
         {parentOption}
@@ -18,7 +31,12 @@ const ToggleCheckbox = ({
       {isSelected && (
         <div className="mt-[3px] ml-5 flex flex-col gap-[4px]">
           {childrenOptions.map(({ id, label }) => (
-            <Checkbox key={id} className="text-[16px]">
+            <Checkbox
+              key={id}
+              checked={selectedChildId === id}
+              onChange={() => handleChildClick(id)}
+              className="text-[16px]"
+            >
               {label}
             </Checkbox>
           ))}
