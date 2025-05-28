c67e5fc7849e20e886f8ddc791d6fc32d6f49997
devjaeyoon <devjaeyoon@gmail.com>
2025-05-28 11:04:56 +0900

    chore: vite-plugin-svgr 플러그인 설치 및 설정 적용

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), svgr(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
