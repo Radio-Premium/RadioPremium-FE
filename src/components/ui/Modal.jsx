ca82a9a11465b98fb42d21b32f9b8f4b0f4efb6d
h-faber <hfaberlu@gmail.com>
2025-05-29 04:17:43 +0900

    style: 모달 그림자 추가

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -3,7 +3,7 @@ import WarningIcon from "@/assets/svgs/icon-warning-red.svg?react";
 const Modal = ({ children, title, subTitle }) => {
   return (
     <div className="fixed inset-0 z-500 bg-white/30 backdrop-blur-sm">
-      <div className="absolute top-[50%] left-[50%] z-600 translate-x-[-50%] translate-y-[-50%] rounded-xl bg-white p-4 px-[25px] py-[20px] shadow-md">
+      <div className="absolute top-[50%] left-[50%] z-600 translate-x-[-50%] translate-y-[-50%] rounded-xl bg-white p-4 px-[25px] py-[20px] shadow-lg">
         <div className="pb-[10px]">
           <WarningIcon className="mx-auto h-[45px] w-[45px]" />
         </div>
