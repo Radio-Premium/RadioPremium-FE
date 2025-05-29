4448057b5ecb1b17cf9c2cb8c3e1da87d9a3ae52
devjaeyoon <devjaeyoon@gmail.com>
2025-05-29 21:49:16 +0900

    refactor: ChannelListItem에서 즐겨찾기 여부 분기 제거
    즐겨찾기 채널과 일반 채널 목록 컴포넌트를 분리함에 따라
    ChannelListItem은 일반 채널 전용 컴포넌트로 단순화

--- src/components/ChannelListItem.jsx
+++ src/components/ChannelListItem.jsx
@@ -1,13 +1,6 @@
 import BlankStarIcon from "@/assets/svgs/icon-blank-star.svg?react";
-import DragDropIcon from "@/assets/svgs/icon-drag-drop.svg?react";
-import FilledStarIcon from "@/assets/svgs/icon-filled-star.svg?react";
 
-const ChannelListItem = ({
-  thumbnail,
-  channelName,
-  isFavoriteChannel,
-  onToggleFavorite,
-}) => {
+const ChannelListItem = ({ thumbnail, channelName, onToggleFavorite }) => {
   return (
     <li className="my-2 flex h-16 w-full items-center rounded-md bg-gray-100 p-2">
       <img
@@ -15,26 +8,10 @@ const ChannelListItem = ({
         src={thumbnail}
         alt={`${channelName} 썸네일`}
       />
-      {isFavoriteChannel ? (
-        <>
-          <p className="ml-3 w-3/5 flex-1 text-sm font-bold">{channelName}</p>
-          <div className="flex flex-1 justify-center pr-2">
-            <button onClick={onToggleFavorite}>
-              <FilledStarIcon className="mr-1 mb-0.5 ml-2" />
-            </button>
-            <button>
-              <DragDropIcon className="ml-3" />
-            </button>
-          </div>
-        </>
-      ) : (
-        <>
-          <p className="ml-3 w-3/4 text-sm font-bold">{channelName}</p>
-          <button onClick={onToggleFavorite}>
-            <BlankStarIcon className="mx-3 ml-auto" />
-          </button>
-        </>
-      )}
+      <p className="ml-3 w-3/4 text-sm font-bold">{channelName}</p>
+      <button onClick={onToggleFavorite}>
+        <BlankStarIcon className="mx-3 ml-auto" />
+      </button>
     </li>
   );
 };
