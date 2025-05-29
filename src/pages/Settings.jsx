2cc39be2721970d8e19a01a313df40768d8cf07a
devjaeyoon <devjaeyoon@gmail.com>
2025-05-29 10:46:56 +0900

    feat: 설정 페이지 UI 구현
    - 광고 감지 및 기존 채널 복귀 기능용 토글 UI 추가
    - TabBar 및 ToggleButton 컴포넌트 적용

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,42 @@
+import TabBar from "@/components/TabBar";
+import ToggleButton from "@/components/ToggleButton";
+
 const Settings = () => {
-  return <>Settings</>;
+  return (
+    <>
+      <TabBar />
+      <div className="p-4">
+        <ul>
+          <li className="my-4 flex h-[88px] w-full min-w-80 rounded-md bg-gray-100 p-2">
+            <section className="w-72">
+              <strong className="text-lg">광고 감지</strong>
+              <p className="mb-[-4px] text-[#888888]">
+                현재 채널에서 광고를 자동으로 감지합니다.
+              </p>
+              <p className="text-[#888888]">
+                광고가 감지되면 다른 채널로 이동합니다.
+              </p>
+            </section>
+            <section className="mx-auto my-auto">
+              <ToggleButton />
+            </section>
+          </li>
+          <li className="my-4 flex h-[88px] w-full min-w-80 rounded-md bg-gray-100 p-2">
+            <section className="w-72">
+              <strong className="text-lg">기존 채널로 이동</strong>
+              <p className="mb-[-4px] text-[#888888]">
+                이전 채널의 광고가 종료되면
+              </p>
+              <p className="text-[#888888]">자동으로 복귀합니다.</p>
+            </section>
+            <section className="mx-auto my-auto">
+              <ToggleButton />
+            </section>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Settings;
