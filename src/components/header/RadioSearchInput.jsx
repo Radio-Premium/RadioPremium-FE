4880b68f2442f3c8dffeaf3f9fe0d3a578c8bb71
Eun0713 <daeunlee0713@naver.com>
2025-05-29 22:51:53 +0900

    refactor: 불필요한 Fragment 제거

--- src/components/header/RadioSearchInput.jsx
+++ src/components/header/RadioSearchInput.jsx
@@ -8,30 +8,28 @@ const RadioSearchInput = () => {
   const [isInputClicked, setIsInputClicked] = useState(false);
 
   return (
-    <>
-      <div className="mt-10 ml-5 flex w-[348px] items-center gap-2">
-        <div className="flex h-11 flex-1 items-center rounded-4xl bg-neutral-200 px-4">
-          <SearchThinIcon className="mr-2" />
-          <input
-            onFocus={() => {
-              setIsInputClicked(true);
-            }}
-            onBlur={() => {
-              setIsInputClicked(false);
-            }}
-            type="text"
-            placeholder={isInputClicked === true ? " " : "Search by radio name"}
-            className="w-full text-sm font-semibold text-neutral-800 outline-none"
-          />
-        </div>
-        <button
-          className="cursor-pointer text-lg font-semibold text-neutral-500"
-          onClick={() => navigate(-1)}
-        >
-          취소
-        </button>
+    <div className="mt-10 ml-5 flex w-[348px] items-center gap-2">
+      <div className="flex h-11 flex-1 items-center rounded-4xl bg-neutral-200 px-4">
+        <SearchThinIcon className="mr-2" />
+        <input
+          onFocus={() => {
+            setIsInputClicked(true);
+          }}
+          onBlur={() => {
+            setIsInputClicked(false);
+          }}
+          type="text"
+          placeholder={isInputClicked === true ? " " : "Search by radio name"}
+          className="w-full text-sm font-semibold text-neutral-800 outline-none"
+        />
       </div>
-    </>
+      <button
+        className="cursor-pointer text-lg font-semibold text-neutral-500"
+        onClick={() => navigate(-1)}
+      >
+        취소
+      </button>
+    </div>
   );
 };
 
