7c75b62c1e69f1c8184698eaeedd6fd22b24e7cf
devjaeyoon <devjaeyoon@gmail.com>
2025-05-29 10:33:21 +0900

    style: ToggleButton 컴포넌트 크기 축소
    기존 크기가 다소 컸기에 높이와 너비를 소폭 축소함

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -10,13 +10,13 @@ const ToggleButton = () => {
   return (
     <>
       <button
-        className={`relative h-9 w-17 cursor-pointer rounded-full transition-colors duration-200 ${
+        className={`relative h-8 w-15 cursor-pointer rounded-full transition-colors duration-200 ${
           isActive ? "bg-black" : "bg-neutral-300"
         }`}
         onClick={handleToggle}
       >
         <div
-          className={`absolute top-1 left-1 h-7 w-7 rounded-full bg-white duration-400 ${
+          className={`absolute top-1 left-1 h-6 w-6 rounded-full bg-white duration-400 ${
             isActive ? "translate-x-8" : "translate-x-0"
           }`}
         />
