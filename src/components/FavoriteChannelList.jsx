44ab90ad5e547563a3b8a92c37158edd1b31914a
devjaeyoon <devjaeyoon@gmail.com>
2025-05-29 21:46:22 +0900

    feat: 즐겨찾기 채널 리스트 드래그 앤 드롭 정렬 기능 구현
    @dnd-kit을 활용하여 즐겨찾기 채널 목록의 우선순위를 변경할 수 있도록
    드래그 앤 드롭 기능이 적용된 FavoriteChannelList 컴포넌트 구현

---
+++ src/components/FavoriteChannelList.jsx
@@ -0,0 +1,61 @@
+import { closestCenter, DndContext } from "@dnd-kit/core";
+import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { useState } from "react";
+
+import FavoriteChannelListItem from "@/components/FavoriteChannelListItem";
+
+const FavoriteChannelList = ({ channelList }) => {
+  const [favoriteChannelList, setFavoriteChannelList] = useState(channelList);
+
+  const handleChannelDragEnd = (event) => {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      const oldIndex = favoriteChannelList.findIndex(
+        (channel) => channel.id === active.id
+      );
+      const newIndex = favoriteChannelList.findIndex(
+        (channel) => channel.id === over.id
+      );
+
+      const newList = arrayMove(favoriteChannelList, oldIndex, newIndex);
+      setFavoriteChannelList(newList);
+    }
+  };
+
+  const restrictToYRange = (args) => {
+    const { transform } = args;
+
+    const minY = -190;
+    const maxY = 190;
+
+    return {
+      ...transform,
+      y: Math.min(Math.max(transform.y, minY), maxY),
+    };
+  };
+
+  return (
+    <DndContext
+      collisionDetection={closestCenter}
+      onDragEnd={handleChannelDragEnd}
+      modifiers={[restrictToVerticalAxis, restrictToYRange]}
+    >
+      <SortableContext
+        items={favoriteChannelList}
+        strategy={verticalListSortingStrategy}
+      >
+        {favoriteChannelList.map(({ id, name }) => (
+          <FavoriteChannelListItem key={id} id={id} name={name} />
+        ))}
+      </SortableContext>
+    </DndContext>
+  );
+};
+
+export default FavoriteChannelList;
