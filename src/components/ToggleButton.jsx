9696fb8532d2fec6bfbc2dc33c131a7a241d20f7
devjaeyoon <devjaeyoon@gmail.com>
2025-05-29 12:59:29 +0900

    style: 토글 버튼 width 수정

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -10,7 +10,7 @@ const ToggleButton = () => {
   return (
     <>
       <button
-        className={`relative h-8 w-15 cursor-pointer rounded-full transition-colors duration-200 ${
+        className={`relative h-8 w-16 cursor-pointer rounded-full transition-colors duration-200 ${
           isActive ? "bg-black" : "bg-neutral-300"
         }`}
         onClick={handleToggle}
