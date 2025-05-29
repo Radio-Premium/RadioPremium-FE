4c42a723896269ab12287d500b29bcf2a0bed01a
devjaeyoon <devjaeyoon@gmail.com>
2025-05-29 20:38:37 +0900

    style: 아이콘 파일명 변경 및 ChannelListItem 레이아웃 수정
    - kebab-case로 변경된 SVG 파일명 컴포넌트 내 반영
    - flex 레이아웃 개선

--- src/components/ChannelListItem.jsx
+++ src/components/ChannelListItem.jsx
@@ -1,6 +1,6 @@
-import BlankStar from "@/assets/svgs/BlankStar.svg?react";
-import DragDrop from "@/assets/svgs/DragDrop.svg?react";
-import FilledStar from "@/assets/svgs/FilledStar.svg?react";
+import BlankStarIcon from "@/assets/svgs/icon-blank-star.svg?react";
+import DragDropIcon from "@/assets/svgs/icon-drag-drop.svg?react";
+import FilledStarIcon from "@/assets/svgs/icon-filled-star.svg?react";
 
 const ChannelListItem = ({
   thumbnail,
@@ -9,7 +9,7 @@ const ChannelListItem = ({
   onToggleFavorite,
 }) => {
   return (
-    <li className="my-2 flex h-16 w-[352px] items-center rounded-md bg-gray-100 p-2">
+    <li className="my-2 flex h-16 w-full items-center rounded-md bg-gray-100 p-2">
       <img
         className="h-12 w-12"
         src={thumbnail}
@@ -17,13 +17,13 @@ const ChannelListItem = ({
       />
       {isFavoriteChannel ? (
         <>
-          <p className="ml-3 w-3/5 text-sm font-bold">{channelName}</p>
-          <div className="flex">
+          <p className="ml-3 w-3/5 flex-1 text-sm font-bold">{channelName}</p>
+          <div className="flex flex-1 justify-center pr-2">
             <button onClick={onToggleFavorite}>
-              <FilledStar className="mr-1 mb-0.5 ml-2" />
+              <FilledStarIcon className="mr-1 mb-0.5 ml-2" />
             </button>
             <button>
-              <DragDrop className="ml-3" />
+              <DragDropIcon className="ml-3" />
             </button>
           </div>
         </>
@@ -31,7 +31,7 @@ const ChannelListItem = ({
         <>
           <p className="ml-3 w-3/4 text-sm font-bold">{channelName}</p>
           <button onClick={onToggleFavorite}>
-            <BlankStar className="mx-3 ml-auto" />
+            <BlankStarIcon className="mx-3 ml-auto" />
           </button>
         </>
       )}
