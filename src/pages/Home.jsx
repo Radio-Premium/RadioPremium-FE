d52da50641da113144c1bdb09702a3cfc62fe609
devjaeyoon <devjaeyoon@gmail.com>
2025-05-29 22:40:07 +0900

    refactor: Home 페이지 채널 영역 공통 레이아웃 컴포넌트로 분리

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import ChannelListItem from "@/components/ChannelListItem";
+import ChannelSection from "@/components/ChannelSection";
 import FavoriteChannelList from "@/components/FavoriteChannelList";
 import TabBar from "@/components/TabBar";
 import useCategorizeChannels from "@/hooks/useCategorizeChannel";
@@ -11,24 +12,18 @@ const Home = () => {
       <div className="h-12 w-full" />
       <TabBar />
       <div className="px-4 pt-8">
-        <section>
-          <h2 className="text-[16px] font-semibold">Favorite Channel</h2>
-          <ul className="scrollbar-hide flex h-60 w-full flex-col items-center overflow-scroll">
-            <FavoriteChannelList channelList={favoriteChannelList} />
-          </ul>
-        </section>
-        <section>
-          <h2 className="text-[16px] font-semibold">Channel</h2>
-          <ul className="scrollbar-hide flex h-60 w-full flex-col items-center overflow-scroll">
-            {channelList.map(({ channelId, name, logoUrl }) => (
-              <ChannelListItem
-                key={channelId}
-                channelName={name}
-                thumbnail={logoUrl}
-              />
-            ))}
-          </ul>
-        </section>
+        <ChannelSection title="Favorite Channel">
+          <FavoriteChannelList channelList={favoriteChannelList} />
+        </ChannelSection>
+        <ChannelSection title="Channel">
+          {channelList.map(({ channelId, name, logoUrl }) => (
+            <ChannelListItem
+              key={channelId}
+              channelName={name}
+              thumbnail={logoUrl}
+            />
+          ))}
+        </ChannelSection>
       </div>
     </>
   );
