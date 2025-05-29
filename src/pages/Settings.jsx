c06a40d5e36e50dd33188020ac043af9c2fdc2ad
devjaeyoon <devjaeyoon@gmail.com>
2025-05-29 13:22:55 +0900

    refactor: 설정 항목 컴포넌트 분리 및 반복 렌더링 구조로 리팩토링
    - 중복된 설정 항목 UI를 SettingListItem 컴포넌트로 분리
    - 설정 정보를 배열로 추상화하여 map 기반 렌더링 구현

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,21 @@
+import SettingListItem from "@/components/SettingListItem";
 import TabBar from "@/components/TabBar";
-import ToggleButton from "@/components/ToggleButton";
+
+const settingList = [
+  {
+    type: "isAdDetect",
+    title: "광고 감지",
+    explanations: [
+      "현재 채널에서 광고를 자동으로 감지합니다.",
+      "광고가 감지되면 다른 채널로 이동합니다.",
+    ],
+  },
+  {
+    type: "isAdDetect",
+    title: "기존 채널로 이동",
+    explanations: ["이전 채널의 광고가 종료되면", "자동으로 복귀합니다."],
+  },
+];
 
 const Settings = () => {
   return (
@@ -7,32 +23,13 @@ const Settings = () => {
       <TabBar />
       <div className="p-4">
         <ul>
-          <li className="my-4 flex h-[88px] w-full min-w-80 rounded-md bg-gray-100 p-2">
-            <section className="w-72">
-              <strong className="text-lg">광고 감지</strong>
-              <p className="mb-[-4px] text-[#888888]">
-                현재 채널에서 광고를 자동으로 감지합니다.
-              </p>
-              <p className="text-[#888888]">
-                광고가 감지되면 다른 채널로 이동합니다.
-              </p>
-            </section>
-            <section className="mx-auto my-auto">
-              <ToggleButton />
-            </section>
-          </li>
-          <li className="my-4 flex h-[88px] w-full min-w-80 rounded-md bg-gray-100 p-2">
-            <section className="w-72">
-              <strong className="text-lg">기존 채널로 이동</strong>
-              <p className="mb-[-4px] text-[#888888]">
-                이전 채널의 광고가 종료되면
-              </p>
-              <p className="text-[#888888]">자동으로 복귀합니다.</p>
-            </section>
-            <section className="mx-auto my-auto">
-              <ToggleButton />
-            </section>
-          </li>
+          {settingList.map(({ type, title, explanations }) => (
+            <SettingListItem
+              key={type}
+              title={title}
+              explanations={explanations}
+            />
+          ))}
         </ul>
       </div>
     </>
