08f0f1e6b97c0977dfe3af1e9b337091145d402a
faber-h <hfaberlu@gmail.com>
2025-05-30 10:05:48 +0900

    feat: 재생 화면 페이지 구현 (#23)
    * feat: 채널 플레이어 UI에 토글 버튼 및 재생/일시정지 기능 추가
    - 채널 변경 여부에 따른 버튼 변경
    - 버튼 컴포넌트 클릭 이벤트 추가
    
    * feat: toggleButton 컴포넌트 size 조절 기능 추가
    
    * refactor: 불필요한 fragment 구조 제거
    
    * refactor: 컴포넌트명에 맞춰 변수명 변경
    
    * rename: 폴더명 수정, 폴더 이동
    - ToggleButton 공통 컴포넌트로 사용하여 ui 폴더로 이동
    - 폴더 이동으로 인한 경로 수정
    - constant 폴더 확장성 고려하여 constants 폴더로 수정
    
    * refactor: isPlaying 사용자 인터랙션으로 재생 시작해야 하므로 초기 값 false로 수정

---
+++ src/components/ui/ToggleButton.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const toggleSize = {
+  s: {
+    buttonSize: "h-6 w-12",
+    thumbSize: "h-4 w-4",
+    activeTranslate: "translate-x-6",
+  },
+  m: {
+    buttonSize: "h-8 w-16",
+    thumbSize: "h-6 w-6",
+    activeTranslate: "translate-x-8",
+  },
+};
+
+const ToggleButton = ({ size = "m" }) => {
+  const [isToggled, setIsToggled] = useState(false);
+
+  const toggleSwitch = () => {
+    setIsToggled((prev) => !prev);
+  };
+
+  const { buttonSize, thumbSize, activeTranslate } = toggleSize[size];
+
+  return (
+    <Button
+      className={`relative cursor-pointer rounded-full transition-colors duration-200 ${
+        isToggled ? "bg-black" : "bg-neutral-300"
+      } ${buttonSize}`}
+      onClick={toggleSwitch}
+    >
+      <div
+        className={`absolute top-1 left-1 rounded-full bg-white duration-400 ${
+          isToggled ? activeTranslate : "translate-x-0"
+        } ${thumbSize}`}
+      />
+    </Button>
+  );
+};
+
+export default ToggleButton;
---
+++ src/pages/ChannelPlayer.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+import MainPauseIcon from "@/assets/svgs/icon-main-pause.svg?react";
+import MainPlayIcon from "@/assets/svgs/icon-main-play.svg?react";
+import Button from "@/components/ui/Button";
+import ToggleButton from "@/components/ui/ToggleButton";
+import { SETTING_TYPES, SETTING_TITLES } from "@/constant/settingOptions";
+
+const ChannelPlayer = ({ isChannelChanged }) => {
+  // 데이터 연결 전 임시 값 할당
+  const logoUrl =
+    "https://www.urbanbrush.net/web/wp-content/uploads/edd/2019/08/urbanbrush-20190805082332272597.png";
+  const channelTitle = "RDO 라디오방송";
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const buttonLabel = isChannelChanged
+    ? SETTING_TITLES[SETTING_TYPES.RETURN_CHANNEL]
+    : SETTING_TITLES[SETTING_TYPES.AD_DETECT];
+
+  const handlePlayPause = () => {
+    setIsPlaying((prev) => !prev);
+  };
+
+  return (
+    <div className="relative mx-auto flex h-[calc(100vh-80px)] max-w-md flex-col px-4 pb-28">
+      <div className="flex flex-1 translate-y-8 flex-col items-center justify-center">
+        <img
+          className="aspect-[268/257] w-[70%] max-w-[268px] rounded-md object-cover"
+          src={logoUrl}
+          alt="채널 로고"
+        />
+        <p className="mt-3.5 text-center text-lg font-bold sm:text-xl">
+          {channelTitle}
+        </p>
+        <div className="mt-2 flex items-center gap-x-2">
+          <p className="text-sm font-semibold sm:text-base">{buttonLabel}</p>
+          <ToggleButton size="s" />
+        </div>
+        <Button className="mt-12" onClick={handlePlayPause}>
+          {isPlaying ? (
+            <MainPauseIcon className="h-[60px] w-[60px] sm:h-[75px] sm:w-[75px]" />
+          ) : (
+            <MainPlayIcon className="h-[60px] w-[60px] sm:h-[75px] sm:w-[75px]" />
+          )}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ChannelPlayer;
