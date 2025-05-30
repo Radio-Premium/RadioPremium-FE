dc6476ae011c1c2af4146465391f1b5ee25db499
Eun0713 <daeunlee0713@naver.com>
2025-05-30 10:48:16 +0900

    style: 검색창 레이아웃 조정

--- src/components/header/RadioSearchInput.jsx
+++ src/components/header/RadioSearchInput.jsx
@@ -8,27 +8,25 @@ const RadioSearchInput = () => {
   const [isInputClicked, setIsInputClicked] = useState(false);
 
   return (
-    <div className="mt-10 ml-5 flex w-[348px] items-center gap-2">
-      <div className="flex h-11 flex-1 items-center rounded-4xl bg-neutral-200 px-4">
-        <SearchThinIcon className="mr-2" />
-        <input
-          onFocus={() => {
-            setIsInputClicked(true);
-          }}
-          onBlur={() => {
-            setIsInputClicked(false);
-          }}
-          type="text"
-          placeholder={isInputClicked ? " " : "Search by radio name"}
-          className="w-full text-sm font-semibold text-neutral-800 outline-none"
-        />
+    <div className="flex w-full justify-center pt-10">
+      <div className="flex w-full max-w-md items-center gap-2 px-4">
+        <div className="flex h-11 flex-1 items-center rounded-4xl bg-neutral-200 px-4">
+          <SearchThinIcon className="mr-2" />
+          <input
+            onFocus={() => setIsInputClicked(true)}
+            onBlur={() => setIsInputClicked(false)}
+            type="text"
+            placeholder={isInputClicked ? " " : "Search by radio name"}
+            className="w-full text-sm font-semibold text-neutral-800 outline-none"
+          />
+        </div>
+        <button
+          className="cursor-pointer text-lg font-semibold text-neutral-500"
+          onClick={() => navigate(-1)}
+        >
+          취소
+        </button>
       </div>
-      <button
-        className="cursor-pointer text-lg font-semibold text-neutral-500"
-        onClick={() => navigate(-1)}
-      >
-        취소
-      </button>
     </div>
   );
 };
