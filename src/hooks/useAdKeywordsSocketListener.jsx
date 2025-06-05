9e9c3aa317482fae45003ef526c6d6f4f4da2b7a
h-faber <hfaberlu@gmail.com>
2025-06-05 15:32:34 +0900

    refactor: 채널 전환 처리 함수로 변경
    - 광고 키워드 클라이언트 판별 로직 제거
    - 광고 탐지 여부(isAd)에 따른 채널 전환 처리 함수 호출

--- src/hooks/useAdKeywordsSocketListener.jsx
+++ src/hooks/useAdKeywordsSocketListener.jsx
@@ -1,34 +1,19 @@
-import { useCallback, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
+import useChannelSwitchOnAd from "@/hooks/useChannelSwitchOnAd";
 import socket from "@/sockets/socketClient";
 
-const adKeywords = ["맥스", "SK브로드밴드", "대리운전"];
-
 const useAdKeywordsSocketListener = () => {
-  const navigate = useNavigate();
-
-  // TODO: 광고 키워드 조회 API 연동해서 adKeywords Mock Data를 응답값으로 변경
-  const checkAdKeywords = (text) => {
-    return adKeywords.some((adKeyword) => adKeyword.includes(text));
-  };
-
-  const handleRadioAdText = useCallback(
-    (text) => {
-      if (checkAdKeywords(text)) {
-        // TODO : 선호채널 목록 받아와서 /channel/:channelId 에 할당
-        navigate("/channel/2");
-      }
-    },
-    [navigate]
-  );
+  const handleChannelSwitch = useChannelSwitchOnAd();
 
   useEffect(() => {
     socket.on("connect", () => {
       console.log("connected");
     });
 
-    socket.on("radioText", handleRadioAdText);
+    socket.on("radioText", ({ isAd }) => {
+      handleChannelSwitch(isAd);
+    });
 
     socket.on("disconnect", () => {
       console.log("disconnect");
@@ -39,7 +24,7 @@ const useAdKeywordsSocketListener = () => {
       socket.off("radioText");
       socket.off("disconnect");
     };
-  }, [handleRadioAdText]);
+  }, [handleChannelSwitch]);
 };
 
 export default useAdKeywordsSocketListener;
