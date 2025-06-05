e1c11aca59530e96aadd9edce3b1a4306f086bc9
Eun0713 <daeunlee0713@naver.com>
2025-06-05 17:06:43 +0900

    refactor: 광고 시간 판별 로직 유틸 함수로 분리

--- src/hooks/useAdTimeDetector.jsx
+++ src/hooks/useAdTimeDetector.jsx
@@ -1,20 +1,6 @@
 import { useEffect, useRef } from "react";
 
-const isAdTimeNow = (minutes, seconds) => {
-  const isPreHalfHourAdTime =
-    (minutes === 27 && seconds >= 30) ||
-    minutes === 28 ||
-    minutes === 29 ||
-    (minutes === 30 && seconds === 0);
-
-  const isPreOnHourAdTime =
-    minutes === 57 ||
-    minutes === 58 ||
-    minutes === 59 ||
-    (minutes === 0 && seconds === 0);
-
-  return isPreHalfHourAdTime || isPreOnHourAdTime;
-};
+import isAdTimeNow from "@/utils/adTimeChecker";
 
 const useAdTimeDetector = (handleChannelSwitch) => {
   const isAdTimeAlreadyHandled = useRef(false);
