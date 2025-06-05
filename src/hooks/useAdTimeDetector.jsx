efdac4d7cf34718728e442af4978b8e2f3ce00e1
Eun0713 <daeunlee0713@naver.com>
2025-06-05 16:41:09 +0900

    refactor: 광고 시간대 변수명 의미 통일

--- src/hooks/useAdTimeDetector.jsx
+++ src/hooks/useAdTimeDetector.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useRef } from "react";
 
 const isAdTimeNow = (minutes, seconds) => {
-  const isPreThirtyAdTime =
+  const isPreHalfHourAdTime =
     (minutes === 27 && seconds >= 30) ||
     minutes === 28 ||
     minutes === 29 ||
     (minutes === 30 && seconds === 0);
 
-  const isPreHourAdTime =
+  const isPreOnHourAdTime =
     minutes === 57 ||
     minutes === 58 ||
     minutes === 59 ||
     (minutes === 0 && seconds === 0);
 
-  return isPreThirtyAdTime || isPreHourAdTime;
+  return isPreHalfHourAdTime || isPreOnHourAdTime;
 };
 
 const useAdTimeDetector = (handleChannelSwitch) => {
