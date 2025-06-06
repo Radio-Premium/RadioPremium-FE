598cb85e89e766f3e20eb6639fb18f8f6e1aa4e7
devjaeyoon <devjaeyoon@gmail.com>
2025-06-06 11:42:06 +0900

    feat: 선택된 채널 ID 전역 상태(selectedChannelId) 및 setter 추가
    - 채널 클릭 시 이동을 위해 selectedChannelId 전역 상태로 관리
    - ChannelPlayer에서 해당 ID를 기반으로 API 요청 예정

--- src/store/useChannelStore.js
+++ src/store/useChannelStore.js
@@ -2,6 +2,8 @@ import { create } from "zustand";
 
 export const useChannelStore = create((set) => ({
   prevChannelId: null,
+  selectedChannelId: null,
 
   setPrevChannelId: (channelId) => set({ prevChannelId: channelId }),
+  setSelectedChannelId: (channelId) => set({ selectedChannelId: channelId }),
 }));
