e45c19db42e9178dea7fc92e3c7d167257f0a494
devjaeyoon <devjaeyoon@gmail.com>
2025-06-06 11:50:02 +0900

    feat: FavoriteChannelListItem 클릭 시 채널 페이지로 이동 기능 추가
    - id prop을 재사용하여 클릭 시 해당 id를 기반으로 이동
    - useChannelNavigation 훅을 활용해 전역 상태 설정 및 페이지 전환 처리

--- src/components/FavoriteChannelListItem.jsx
+++ src/components/FavoriteChannelListItem.jsx
@@ -3,6 +3,7 @@ import { CSS } from "@dnd-kit/utilities";
 
 import DragDropIcon from "@/assets/svgs/icon-drag-drop.svg?react";
 import FilledStarIcon from "@/assets/svgs/icon-filled-star.svg?react";
+import useChannelNavigation from "@/hooks/useChannelNavigation";
 
 const FavoriteChannelListItem = ({ id, name }) => {
   const {
@@ -14,6 +15,7 @@ const FavoriteChannelListItem = ({ id, name }) => {
     transform,
     transition,
   } = useSortable({ id });
+  const navigateToChannelPlayer = useChannelNavigation();
 
   const style = {
     transform: CSS.Transform.toString(transform),
@@ -26,6 +28,7 @@ const FavoriteChannelListItem = ({ id, name }) => {
       style={style}
       {...attributes}
       className="my-2 flex h-16 w-full items-center rounded-md bg-gray-100 p-2 select-none"
+      onClick={() => navigateToChannelPlayer(id)}
     >
       <div className="h-12 w-12 flex-none rounded-lg bg-gray-200" />
       <p className="ml-3 w-3/5 flex-1 truncate overflow-hidden text-sm font-bold whitespace-nowrap">
