c0942fe1cc29c59ae65c5f66011e6d582eb5ef3c
devjaeyoon <devjaeyoon@gmail.com>
2025-06-06 14:43:12 +0900

    refactor: channelInfo 초기값 빈 객체에서 null로 변경

--- src/hooks/useChannelInfo.jsx
+++ src/hooks/useChannelInfo.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const useChannelInfo = (channelId) => {
-  const [channelInfo, setChannelInfo] = useState({});
+  const [channelInfo, setChannelInfo] = useState(null);
 
   useEffect(() => {
     const fetchChannelInfo = async () => {
