caf2b88983681c577ef50f22f6acc7e1d7213707
h-faber <hfaberlu@gmail.com>
2025-06-07 14:35:40 +0900

    refactor: API 경로 중복 제거를 위해 BASE_URL 상수로 추출

--- src/apis/interestChannel.js
+++ src/apis/interestChannel.js
@@ -1,19 +1,14 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 export const createInterestChannel = (userId, channelId) =>
-  axios.post(
-    `${import.meta.env.VITE_API_URL}/users/${userId}/interest-channels`,
-    { channelId }
-  );
+  axios.post(`${BASE_URL}/users/${userId}/interest-channels`, { channelId });
 
 export const deleteInterestChannel = (userId, channelId) =>
-  axios.delete(
-    `${import.meta.env.VITE_API_URL}/users/${userId}/interest-channels`,
-    { data: { channelId } }
-  );
+  axios.delete(`${BASE_URL}/users/${userId}/interest-channels`, {
+    data: { channelId },
+  });
 
 export const updateInterestChannels = (userId, channelIds) =>
-  axios.put(
-    `${import.meta.env.VITE_API_URL}/users/${userId}/interest-channels`,
-    { channelIds }
-  );
+  axios.put(`${BASE_URL}/users/${userId}/interest-channels`, { channelIds });
