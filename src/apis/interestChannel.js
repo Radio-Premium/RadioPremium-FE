282f5dac163205c9a73a87853ae155ec96cde0de
h-faber <hfaberlu@gmail.com>
2025-06-07 14:45:10 +0900

    refactor: axios 인스턴스 생성 및 관심 채널 API에 적용
    - baseURL과 공통 headers가 설정된 axios 인스턴스를 생성
    - 관심 채널 관련 API 함수(create, delete, update)에 인스턴스 적용
    - 코드 중복 제거

--- src/apis/interestChannel.js
+++ src/apis/interestChannel.js
@@ -1,14 +1,16 @@
-import axios from "axios";
-
-const BASE_URL = import.meta.env.VITE_API_URL;
+import axiosInstance from "@/apis/axiosInstance";
 
 export const createInterestChannel = (userId, channelId) =>
-  axios.post(`${BASE_URL}/users/${userId}/interest-channels`, { channelId });
+  axiosInstance.post(`/users/${userId}/interest-channels`, {
+    channelId,
+  });
 
 export const deleteInterestChannel = (userId, channelId) =>
-  axios.delete(`${BASE_URL}/users/${userId}/interest-channels`, {
+  axiosInstance.delete(`/users/${userId}/interest-channels`, {
     data: { channelId },
   });
 
 export const updateInterestChannels = (userId, channelIds) =>
-  axios.put(`${BASE_URL}/users/${userId}/interest-channels`, { channelIds });
+  axiosInstance.put(`/users/${userId}/interest-channels`, {
+    channelIds,
+  });
