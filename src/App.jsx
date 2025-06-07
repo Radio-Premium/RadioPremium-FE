c8245ae53ee76b409b2432e8377ce82921ba1d75
이재윤 <devjaeyoon@gmail.com>
2025-06-07 19:24:14 +0900

    chore: tanstack query 설치 (#64)
    * chore: @tanstack/react-query 설치
    
    * chore: @tanstack/eslint-plugin-query 설치
    
    * chore: @tanstack/eslint-plugin-query eslint.config.js에 적용
    
    * feat: QueryClient 및 QueryClientProivder 추가
    
    * chore: @tanstack/react-query-devtools 설치
    
    * feat: ReactQueryDevtools 추가
    
    * feat: ReactQueryDevtools를 개발 환경에서만 렌더링하도록 변경
    
    - Vite의 import.meta.env.DEV를 사용해 Devtools를 개발 환경에서만 렌더링
    - production 빌드에서 Devtools 코드가 번들에 포함되지 않도록 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
 
 import router from "@/routes";
 
+const queryClient = new QueryClient();
+
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
+  );
 };
 
 export default App;
