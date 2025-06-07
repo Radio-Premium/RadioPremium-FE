22cdb022943b365c76e21619f7c21b50577eff04
devjaeyoon <devjaeyoon@gmail.com>
2025-06-07 18:22:19 +0900

    chore: @tanstack/eslint-plugin-query eslint.config.js에 적용

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import js from "@eslint/js";
+import queryPlugin from "@tanstack/eslint-plugin-query";
 import importPlugin from "eslint-plugin-import";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -25,6 +26,7 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      ...queryPlugin.configs["flat/recommended"].rules,
       "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
       "react-refresh/only-export-components": [
         "warn",
