d5854e5559e772f5a5bca51c6d6bca260e29b83f
h-faber <hfaberlu@gmail.com>
2025-06-08 16:00:33 +0900

    refactor: MiniPlayer에 광고 감지 설정값 반영
    - ChannelPlayer와 동일한 로직 흐름으로 변경
    - useUserStore에서 isAdDetect 가져와서 재생 함수에 전달

--- src/components/MiniPlayer.jsx
+++ src/components/MiniPlayer.jsx
@@ -1,28 +1,35 @@
-import { useState, useRef } from "react";
+import { useRef } from "react";
 
 import CloseIcon from "@/assets/svgs/icon-close.svg?react";
 import PauseIcon from "@/assets/svgs/icon-mini-pause.svg?react";
 import PlayIcon from "@/assets/svgs/icon-mini-player.svg?react";
+import { SETTING_TYPES } from "@/constants/settingOptions";
+import { useChannelStore } from "@/store/useChannelStore";
+import { useUserStore } from "@/store/useUserStore";
 import controlStreamingPlayback from "@/utils/playControl";
 
-const MiniPlayer = ({ thumbnail, channelName, closePlayer }) => {
-  const [isPlaying, setIsPlaying] = useState(false);
+const MiniPlayer = ({ closePlayer }) => {
+  const { selectedChannelId, radioChannelList, isPlaying, setIsPlaying } =
+    useChannelStore();
+  const { settings } = useUserStore();
+  const isAdDetect = settings[SETTING_TYPES.AD_DETECT];
+  const channel = radioChannelList.find(
+    (channel) => channel.id === selectedChannelId
+  );
   const videoId = useRef(null);
 
+  const { name, logoUrl } = channel;
+
   const handlePlayPause = () => {
-    controlStreamingPlayback(videoId, !isPlaying);
+    controlStreamingPlayback(videoId, selectedChannelId, isPlaying, isAdDetect);
     setIsPlaying((prev) => !prev);
   };
 
   return (
     <div className="flex h-20 w-full justify-between rounded-b-4xl bg-white px-4 shadow-[0_-6px_9px_rgba(0,0,0,0.3)]">
       <div className="flex items-center">
-        <img
-          className="ml-2 h-16 w-16"
-          src={thumbnail}
-          alt={`${channelName} 썸네일`}
-        />
-        <p className="ml-2 text-sm font-black">{channelName}</p>
+        <img className="ml-2 h-16 w-16" src={logoUrl} alt={`${name} 썸네일`} />
+        <p className="ml-2 text-sm font-black">{name}</p>
       </div>
       <video ref={videoId} className="hidden" />
       <div className="flex items-center gap-3">
