dc6b7f49851f6b8090746b29f041a2b53898840d
heeju <hfaberlu@gmail.com>
2025-06-08 12:44:43 +0900

    refactor: 플레이 버튼 클릭 시 사용자 정보 조회 API 연동하도록 동작 변경 (#71)
    * refactor: 채널 재생 시 API 직접 호출로 변경
    
    - 채널 재생 버튼 클릭 시 API 직접 호출로 변경
    - 플레이 여부 Zustand 상태 추가
    - controlStreamingPlayback 내에서 getChannelInfo API 직접 호출하도록 유지
    - useChannelInfo 훅은 사용되지 않아 제거
    
    * feat: zustand에 persist 미들웨어 적용하여 상태 새로고침 시 유지
    
    * fix: 선택된 채널 정보를 정확히 가져오도록 filter → find로 수정
    
    - 선택한 채널 정보가 undefined로 나오는 문제 해결
    
    * fix: useState의 업데이트 함수에서 Zustand set함수에 맞게 변경
    
    * fix: HLS 중복 스트림 생성을 방지하기 위해 기존 인스턴스 제거 로직 추가
    
    * refactor: zustand 이전 상태 기반 업데이트 가능하도록 변경

--- src/store/useChannelStore.js
+++ src/store/useChannelStore.js
@@ -1,14 +1,23 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useChannelStore = create((set) => ({
-  interestChannelIds: [],
-  prevChannelId: null,
-  radioChannelList: [],
-  selectedChannelId: null,
+export const useChannelStore = create(
+  persist((set) => ({
+    interestChannelIds: [],
+    prevChannelId: null,
+    radioChannelList: [],
+    selectedChannelId: null,
+    isPlaying: false,
 
-  setInterestChannelIds: (interestChannelIds) =>
-    set({ interestChannelIds: interestChannelIds }),
-  setPrevChannelId: (channelId) => set({ prevChannelId: channelId }),
-  setRadioChannelList: (channelList) => set({ radioChannelList: channelList }),
-  setSelectedChannelId: (channelId) => set({ selectedChannelId: channelId }),
-}));
+    setInterestChannelIds: (interestChannelIds) =>
+      set({ interestChannelIds: interestChannelIds }),
+    setPrevChannelId: (channelId) => set({ prevChannelId: channelId }),
+    setRadioChannelList: (channelList) =>
+      set({ radioChannelList: channelList }),
+    setSelectedChannelId: (channelId) => set({ selectedChannelId: channelId }),
+    setIsPlaying: (updater) =>
+      typeof updater === "function"
+        ? set((state) => ({ isPlaying: updater(state.isPlaying) }))
+        : set({ isPlaying: updater }),
+  }))
+);
--- src/utils/playControl.js
+++ src/utils/playControl.js
@@ -1,21 +1,39 @@
 import Hls from "hls.js";
 
+import { getChannelInfo } from "@/apis/radioChannels";
+
 let hlsInstance = null;
 
-const controlStreamingPlayback = (videoId, videoUrl, isPlaying) => {
-  const video = videoId.current;
+const controlStreamingPlayback = async (
+  videoRef,
+  selectedChannelId,
+  isPlaying
+) => {
+  const video = videoRef.current;
 
-  if (isPlaying) {
-    if (Hls.isSupported()) {
-      hlsInstance = new Hls();
-      hlsInstance.loadSource(videoUrl);
-      hlsInstance.attachMedia(video);
-      hlsInstance.on(Hls.Events.MANIFEST_PARSED, () => {
+  if (!isPlaying) {
+    try {
+      const { data } = await getChannelInfo(selectedChannelId);
+      const url = data.url;
+
+      if (hlsInstance) {
+        hlsInstance.destroy();
+        hlsInstance = null;
+      }
+
+      if (Hls.isSupported()) {
+        hlsInstance = new Hls();
+        hlsInstance.loadSource(url);
+        hlsInstance.attachMedia(video);
+        hlsInstance.on(Hls.Events.MANIFEST_PARSED, () => {
+          video.play();
+        });
+      } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+        video.src = url;
         video.play();
-      });
-    } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-      video.src = videoUrl;
-      video.play();
+      }
+    } catch (error) {
+      console.error("fetch channelInfo failed", error);
     }
   } else {
     video.pause();
