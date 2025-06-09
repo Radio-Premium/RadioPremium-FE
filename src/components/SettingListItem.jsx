152591c623a7ca88108525537e75cfac2f2d06ba
Eun0713 <daeunlee0713@naver.com>
2025-06-09 23:59:35 +0900

    fix: 광고 안내 문구 잘림 문제 해결을 위한 스타일 조정

--- src/components/SettingListItem.jsx
+++ src/components/SettingListItem.jsx
@@ -13,13 +13,13 @@ const SettingListItem = ({ type, title, explanations }) => {
 
   return (
     <li className="my-4 flex h-[100px] w-full min-w-80 rounded-md bg-gray-100 px-4 pt-4">
-      <div className="w-[272px]">
+      <div className="w-[280px]">
         <h2 className="text-lg font-semibold">{title}</h2>
         {explanations.map((explanation, index) =>
           index === 0 ? (
             <p
               key={index}
-              className="mt-1 mb-[-4px] text-[14px] text-[#888888]"
+              className="mt-1 mb-[-2px] text-[14px] text-[#888888]"
             >
               {explanation}
             </p>
