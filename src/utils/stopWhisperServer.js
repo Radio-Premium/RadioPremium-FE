dd6214833d6e095873a734b6f593e8bcc4a3dc7d
h-faber <hfaberlu@gmail.com>
2025-06-13 00:22:53 +0900

    chore: Whisper 관련 요청 코드에서 VITE_WHISPER_API_URL 참조하도록 수정

--- src/utils/stopWhisperServer.js
+++ src/utils/stopWhisperServer.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const stopWhisperServer = async (userId) => {
   try {
-    // 추후 배포 시 변경
-    await axios.post("http://localhost:5000/stop", { userId });
+    const whisperServerURL = import.meta.env.VITE_WHISPER_API_URL;
+    await axios.post(`${whisperServerURL}/stop`, { userId });
   } catch (error) {
     console.error("❌ Whisper 종료 실패", error);
   }
