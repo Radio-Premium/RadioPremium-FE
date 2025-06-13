9cd48efce582780a6b697d76b09e3cf4d70a0623
h-faber <hfaberlu@gmail.com>
2025-06-13 15:42:57 +0900

    feat: 광고 감지 이동 채널 선택 컴포넌트 AdRedirectChannelItem 추가

---
+++ src/components/AdRedirectChannelItem.jsx
@@ -0,0 +1,33 @@
+import SelectIcon from "@/assets/svgs/icon-select.svg?react";
+import Button from "@/components/ui/Button";
+
+const AdRedirectChannelItem = ({
+  channelId,
+  channelName,
+  thumbnail,
+  isSelected,
+  onSelect,
+}) => {
+  const borderColor = `${isSelected ? "border-[#5b4dff]" : "border-[#e0dfdf]"}`;
+
+  return (
+    <li
+      className={`${borderColor} my-2 flex h-16 w-full items-center rounded-md border bg-white px-6 py-2 select-none`}
+      onClick={() => onSelect(channelId)}
+    >
+      <img
+        className="h-12 w-12 rounded-[50%]"
+        src={thumbnail}
+        alt={`${channelName} 썸네일`}
+      />
+      <p className="ml-3 w-3/4 text-sm font-bold">{channelName}</p>
+      {isSelected && (
+        <Button>
+          <SelectIcon className="ml-3" />
+        </Button>
+      )}
+    </li>
+  );
+};
+
+export default AdRedirectChannelItem;
