788aca3f7145c008ba76d5b9df3b68bab62b9a76
DaEun <129145396+Eun0713@users.noreply.github.com>
2025-06-13 18:01:20 +0900

    feat: UI 개선 및 전체 텍스트 한국어화 작업 (#127)
    * feat: 채널 관련 UI 텍스트 및 검색창 안내 문구 한국어로 변경
    
    * fix: 즐겨찾기 빈 별 잘림 현상 수정
    
    * feat: 채널 검색 페이지 취소 버튼을 뒤로가기 아이콘으로 교체
    
    * feat: 광고 제보 확인 버튼에 disabled 스타일 추가
    
    * refactor: 채널 검색 화면 뒤로가기 버튼에 공통 Button 컴포넌트 적용

--- src/components/header/RadioSearchInput.jsx
+++ src/components/header/RadioSearchInput.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import BackArrow from "@/assets/svgs/icon-back-arrow.svg?react";
 import SearchThinIcon from "@/assets/svgs/icon-search-thin.svg?react";
+import Button from "@/components/ui/Button";
 import { useSearchStore } from "@/store/useSearchStore";
 
 const RadioSearchInput = () => {
@@ -13,6 +15,9 @@ const RadioSearchInput = () => {
   return (
     <div className="flex w-full justify-center pt-10">
       <div className="flex w-full max-w-md items-center gap-2 px-4">
+        <Button className="mr-4 cursor-pointer" onClick={() => navigate(-1)}>
+          <BackArrow />
+        </Button>
         <div className="flex h-11 flex-1 items-center rounded-4xl bg-neutral-200 px-4">
           <SearchThinIcon className="mr-2" />
           <input
@@ -21,16 +26,10 @@ const RadioSearchInput = () => {
             onBlur={() => setIsInputClicked(false)}
             onChange={(e) => setKeyword(e.target.value)}
             type="text"
-            placeholder={!isInputClicked && "Search by radio name"}
+            placeholder={!isInputClicked && "채널 이름을 입력해 주세요"}
             className="w-full text-sm font-semibold text-neutral-800 outline-none"
           />
         </div>
-        <button
-          className="cursor-pointer text-lg font-semibold text-neutral-500"
-          onClick={() => navigate(-1)}
-        >
-          취소
-        </button>
       </div>
     </div>
   );
