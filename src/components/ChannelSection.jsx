671d76e017ade0631e07a791bb034960ac81b46e
h-faber <hfaberlu@gmail.com>
2025-06-13 15:43:27 +0900

    feat: ChannelSection 컴포넌트에 subTitleList prop 추가

--- src/components/ChannelSection.jsx
+++ src/components/ChannelSection.jsx
@@ -1,7 +1,20 @@
-const ChannelSection = ({ title, children, marginTop, height }) => {
+const ChannelSection = ({
+  title,
+  subTitleList,
+  children,
+  marginTop,
+  height,
+}) => {
   return (
     <section>
       <h2 className={`mb-2 text-base font-semibold ${marginTop}`}>{title}</h2>
+      {subTitleList && (
+        <div className="mt-1 text-sm text-[#888888]">
+          {subTitleList.map((subtitle) => (
+            <p>{subtitle}</p>
+          ))}
+        </div>
+      )}
       <ul
         className={`scrollbar-hide flex w-full flex-col items-center overflow-scroll ${height}`}
       >
