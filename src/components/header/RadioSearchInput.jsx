68790167db4a3e120d27d822836e4af99ebcbd24
Eun0713 <daeunlee0713@naver.com>
2025-06-13 17:12:45 +0900

    feat: 채널 검색 페이지 취소 버튼을 뒤로가기 아이콘으로 교체

--- src/components/header/RadioSearchInput.jsx
+++ src/components/header/RadioSearchInput.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import BackArrow from "@/assets/svgs/icon-back-arrow.svg?react";
 import SearchThinIcon from "@/assets/svgs/icon-search-thin.svg?react";
 import { useSearchStore } from "@/store/useSearchStore";
 
@@ -13,6 +14,9 @@ const RadioSearchInput = () => {
   return (
     <div className="flex w-full justify-center pt-10">
       <div className="flex w-full max-w-md items-center gap-2 px-4">
+        <button className="mr-4 cursor-pointer" onClick={() => navigate(-1)}>
+          <BackArrow />
+        </button>
         <div className="flex h-11 flex-1 items-center rounded-4xl bg-neutral-200 px-4">
           <SearchThinIcon className="mr-2" />
           <input
@@ -25,12 +29,6 @@ const RadioSearchInput = () => {
             className="w-full text-sm font-semibold text-neutral-800 outline-none"
           />
         </div>
-        <button
-          className="cursor-pointer text-lg font-semibold text-neutral-500"
-          onClick={() => navigate(-1)}
-        >
-          취소
-        </button>
       </div>
     </div>
   );
